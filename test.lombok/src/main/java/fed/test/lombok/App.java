51fafa8b36291d8fe0990d7f4fdbd063b14c7e95
FeDLviv <fed.lviv@gmail.com>
2017-12-08 13:48:21 +0200

    Delete old files

--- test.lombok/src/main/java/fed/test/lombok/App.java
+++
@@ -1,9 +0,0 @@
-package fed.test.lombok;
-
-public class App {
-	public static void main(String[] args) {
-		Auto x = new Auto("Audi", 200, 1.45);
-		x.setSpeed(220);
-		System.out.println(x);
-	}
-}
