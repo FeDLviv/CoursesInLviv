55992ed87be6128498d5c33ec42c2726f364e1ed
fedlviv <fed.lviv@gmail.com>
2018-01-07 23:16:18 +0200

    Add project social facebook from IDEA

---
+++ idea-face/src/main/java/com/example/ideaface/IdeaFaceApplication.java
@@ -0,0 +1,12 @@
+package com.example.ideaface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaFaceApplication.class, args);
+	}
+}
